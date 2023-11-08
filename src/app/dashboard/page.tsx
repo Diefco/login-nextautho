400a566d92c378edbf81576cebbf9824bf321c58
Diefco <diefco.web@gmail.com>
2023-11-08 10:24:39 -0500

    pages: home & dashboard

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,20 @@
+"use client";
+import { signOut } from "next-auth/react";
+
+function DashboardPage() {
+  return (
+    <section className="h-[calc(100vh-7rem)] flex flex-col justify-center items-center">
+      <h1 className="text-white text-5xl">Dashboard</h1>
+      <button
+        className="bg-white text-black px-4 py-2 mt-4 rounded-md"
+        onClick={() => {
+          signOut();
+        }}
+      >
+        Logout
+      </button>
+    </section>
+  );
+}
+
+export default DashboardPage;
